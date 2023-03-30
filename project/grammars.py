2c0ead297c5a89333b1e9877b0cc83b5ac200c8d
Evgeny Svirin <eugenysvirin@gmail.com>
2023-03-30 21:49:36 +0300

    task6 done

---
+++ project/grammars.py
@@ -0,0 +1,28 @@
+from pyformlang.cfg import CFG, epsilon
+
+
+def build_wcnf(cfg: CFG) -> CFG:
+    """Build weak Chomsky normal form from context free grammar
+    perform actions similar to creating normal form:
+    remove unit productions
+    decompose to single terminal head productions
+    decompose to smaller body rules
+    :param cfg: context free grammar to build from
+    :return: weak Chomsky normal form grammar
+    """
+    helperCFG = cfg.eliminate_unit_productions().remove_useless_symbols()
+    new_prodictions = helperCFG._decompose_productions(helperCFG._get_productions_with_only_single_terminals())
+
+    return CFG(productions=new_prodictions,
+               start_symbol=helperCFG.start_symbol).remove_useless_symbols()
+
+
+def read_cfg(filename: str) -> CFG:
+    """
+    Reads context free grammar from file
+    :param filename: path to file
+    :return: context free grammar
+    """
+    with open(filename) as f:
+        return CFG.from_text("".join(f.readlines()))
+
---
+++ tests/test_cfg.py
@@ -0,0 +1,98 @@
+import project.grammars as grams
+
+import pyformlang.cfg
+from pyformlang.cfg.variable import Variable
+from pyformlang.cfg.terminal import Terminal
+from pyformlang.cfg import CFG, epsilon
+from tempfile import NamedTemporaryFile
+
+
+def is_wcnf_product(prod: CFG.productions) -> bool:
+    """
+    Check if product matches weak Chomsky normal form
+    :param prod: production to check
+    :return: True if matches else False
+    """
+    bd = prod.body
+    return len(bd) == 0 or (len(bd) == 1 and (type(bd[0]) == Terminal
+                                              or type(bd[0]) == epsilon)) or (len(bd) == 2 and type(bd[0]) == Variable
+                                                                              and type(bd[1] == Variable))
+
+
+def test_cfg1():
+    cfg = CFG.from_text("""
+    S -> C
+    C -> S C
+    C -> c """)
+
+    assert not all(is_wcnf_product(p) for p in cfg.productions)
+
+    wcnf = grams.build_wcnf(cfg)
+    assert all(is_wcnf_product(p) for p in wcnf.productions)
+
+    start_symbol_righter = False
+    for p in wcnf.productions:
+        if p.body.count(cfg.start_symbol):
+            start_symbol_righter = True
+            break
+    assert start_symbol_righter is True
+
+
+def test_cfg2():
+    cfg = CFG.from_text("""
+    S -> A
+    A -> B C D
+    B -> b 
+    C -> c
+    D -> d
+    B -> S
+    B -> E
+    E -> e 
+    d -> e
+    C -> S C
+    B -> $ """)
+    assert not all(is_wcnf_product(p) for p in cfg.productions)
+
+    wcnf = grams.build_wcnf(cfg)
+    assert all(is_wcnf_product(p) for p in wcnf.productions)
+
+    start_symbol_righter = False
+
+    for p in wcnf.productions:
+        if p.body.count(cfg.start_symbol):
+            start_symbol_righter = True
+            break
+    assert start_symbol_righter is True
+
+
+def test_cfg_read():
+    filename = ""
+    with NamedTemporaryFile(mode="w", delete=False) as temp_file:
+        temp_file.write(
+            """
+            S -> A
+            A -> B C D
+            B -> b 
+            C -> c
+            D -> d
+            B -> S
+            B -> E
+            E -> e 
+            d -> e
+            C -> S C
+            B -> $ """)
+        filename = temp_file.name
+
+    cfg = grams.read_cfg(filename)
+    assert not all(is_wcnf_product(p) for p in cfg.productions)
+
+    wcnf = grams.build_wcnf(cfg)
+    assert all(is_wcnf_product(p) for p in wcnf.productions)
+
+    start_symbol_righter = False
+
+    for p in wcnf.productions:
+        if p.body.count(cfg.start_symbol):
+            start_symbol_righter = True
+            break
+    assert start_symbol_righter is True
